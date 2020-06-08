dd0a84c0df315d5119401bf60c2c1ac8b545e6ac
meltingshine <meltingshine@gmail.com>
2020-06-08 19:45:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fragmenttest_20200608/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fragmenttest_20200608
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
